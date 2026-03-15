6c3342e5694f756527b51726b1ee90d2b0663003
KaterinaZhuvak <katerinazhuvak@icloud.com>
2026-03-15 08:22:04 +0000

    new comit

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
@@ -7,9 +7,9 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-  <StrictMode>
+
     <App />
-  </StrictMode>,
+
   </Provider>
   
 )
